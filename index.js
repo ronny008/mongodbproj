f66e1a782f4b0f6b7c62e32989d5ab8db2d4e981
ronny008 <150226641+ronny008@users.noreply.github.com>
2025-04-07 14:11:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-var express = require('express');
-require(`./db`)
+// importing express 
+var express = require(`express`);
+require('./db')
+var emp= require(`./model/employee`);
 var app = express();
-var port = 4000;
 
-
-// api to add data to db
-var sModel=require(`./model/sample`)
-// middlewar to add to db
+var port = 3999;
+// middleware
 app.use(express.json());
-
-app.post(`/`,(req,res)=>{
+app.post('/',(req,res)=>{
     try {
-        sModel(req.body).save();
-        res.send("Data added")
-        
+        emp(req.body).save();
+        res.send("data added to db") 
     } catch (error) {
-        res.send(err)
-        
+        res.send(error)
         
     }
 })
+// get
 app.get(`/`,async(req,res)=>{
     try {
-        var data = await sModel.find();
+        var data = await emp.find();
         res.send(data)  
     } catch (error) {
         res.send(error)    
@@ -32,7 +29,7 @@ app.get(`/`,async(req,res)=>{
 app.delete(`/:id`,async(req,res)=>{
     try {
         console.log(req.params.id)
-        await sModel.findByIdAndDelete(req.params.id);
+        await emp.findByIdAndDelete(req.params.id);
         res.send("data delected")
         // res.send(id)
     } catch (error) {
@@ -40,6 +37,17 @@ app.delete(`/:id`,async(req,res)=>{
         
     }
 })
+app.put('/:id',async(req,res)=>{
+    try {
+        await emp.findByIdAndUpdate(req.params.id,req.body);
+        res.send("edited")
+    } catch (error) {
+        res.send(error)
+        
+    }
+})
+// to get data
 app.listen(port,()=>{
     console.log(`server is up and running in ${port}`)
+    
 })
